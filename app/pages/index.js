b2b20658a3d78209c256995488cccec34cdfa251
Ammar Hebib <ammar.hebib@gmail.com>
2021-04-13 23:57:59 -0400

    Init nextjs app

---
+++ app/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next App</title>
+      </Head>
+
+      <div>
+          <h1>NextJS Tempalte</h1>
+      </div>
+    </div>
+  )
+}
